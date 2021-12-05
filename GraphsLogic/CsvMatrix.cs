fe42e71f521eb348155703b44092b8236013ca10
WEBMaximWEB <maxim.geer@yandex.ru>
2021-12-05 20:27:24 +0500

    запись и чтение матрицы из файла

---
+++ GraphsLogic/CsvMatrix.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GraphsLogic
+{
+    public static class CsvMatrix
+    {
+        private static double[,] GetMatrix(string path)
+        {
+            string[] lines = File.ReadAllLines(path = Directory.GetCurrentDirectory() + @"\matrix.csv");
+            double[,] matrix = new double[lines.Length, lines.Length];
+            for(int i = 0; i < lines.Length; i++)
+            {
+                int j = 0;
+                lines[i] = lines[i].Replace(" ", "");
+                string temp = "";
+                foreach (char c in lines[i])
+                {
+                    if( c == ';')
+                    {
+                        matrix[i, j] = Int64.Parse(temp);
+                        temp = string.Empty;
+                        j++;
+                        continue;
+                    }
+                    else
+                    {
+                        temp += c;
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        public static void WriteMatrix(string path, double[,] matrix)
+        {
+            File.Delete(path);
+            for(int i = 0; i < Math.Sqrt(matrix.Length); i++)
+            {
+                string temp = string.Empty;
+                for(int j = 0; j < Math.Sqrt(matrix.Length); j++)
+                {
+                    temp += matrix[i,j] + ";";
+                }
+                temp += "\n";
+                File.AppendAllText(path, temp);
+            }
+        }
+
+        public static Graph GetGraph(string path)
+        {
+            double[,] matrix = GetMatrix(path);
+            Graph graph = new Graph();
+            for(int i = 0; i < Math.Sqrt(matrix.Length); i++)
+                graph.Nodes.Add(new Node());
+            for(int i = 0; i < Math.Sqrt(matrix.Length); i++)
+            {
+                for (int j = 0; j < Math.Sqrt(matrix.Length); j++)
+                {
+                    if (matrix[i, j] == 0)
+                        continue;
+                    graph.AddLink(graph.Nodes[i], graph.Nodes[j], matrix[i,j], LinkType.Unidirectional);
+                }
+            }
+            return graph;
+        }
+
+        public static void SaveGraph(Graph graph, string path)
+        {
+            double[,] matrix = new double[graph.Nodes.Count, graph.Nodes.Count];
+            for (int i = 0; i < Math.Sqrt(matrix.Length); i++)
+            {
+                for (int j = 0; j < Math.Sqrt(matrix.Length); j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+            for( int i = 0; i < graph.Nodes.Count; i++)
+            {
+                for (int j = 0; j < graph.Nodes[i].Links.Count; j++)
+                {
+                    matrix[i, j] = graph.Nodes[i].Links[j].Value;
+                }
+            }
+        }    
+    }
+}
--- GraphsMG/Program.cs
+++ GraphsMG/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using GraphsLogic;
 
 namespace GraphsMG
 {
@@ -9,6 +11,11 @@ namespace GraphsMG
         {
             using (var game = new Game1())
                 game.Run();
+/*            string path = Directory.GetCurrentDirectory() + @"\matrix.csv";
+            double[,] matrix = { { 1, 2, 3, 4, 5 }, { 1, 2, 0, 0, 5 }, { 0, 0, 3, 4, 0 }, { 1, 2, 0, 4, 5 }, { 0, 0, 0, 4, 5 } };
+            CsvMatrix.WriteMatrix(path, matrix);
+            GraphsLogic.Graph res = CsvMatrix.GetGraph(path);
+            CsvMatrix.SaveGraph(res, path);*/
         }
     }
 }
